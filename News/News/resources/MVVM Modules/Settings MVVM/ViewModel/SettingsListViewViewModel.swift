7cabb6d22b2549c4360c15d32cd8d3092646e9c3
MarkDestroyer <markkir123@gmail.com>
2023-06-03 21:00:59 +0300

    update

--- News/News/resources/MVVM Modules/Settings MVVM/ViewModel/SettingsListViewViewModel.swift
+++ News/News/resources/MVVM Modules/Settings MVVM/ViewModel/SettingsListViewViewModel.swift
@@ -10,6 +10,15 @@ import UIKit
 class SettingsListViewViewModel {
     
     var settings = [
-        SettingsSectionModel(id: 1, name: "Иконка", settings: [SettingsModel(id: 1, name: "газета", icon: "newspaper"), SettingsModel(id: 2, name: "огонь", icon: "fire"), SettingsModel(id: 3, name: "робот", icon: "technology"), SettingsModel(id: 4, name: "мяч", icon: "sport"), SettingsModel(id: 5, name: "монета", icon: "business"), SettingsModel(id: 6, name: "микроскоп", icon: "science"), SettingsModel(id: 7, name: "цирк", icon: "entertainment"), SettingsModel(id: 8, name: "крест", icon: "health")])
+        SettingsSectionModel(id: 1, name: "Иконка", settings: [
+            SettingsModel(id: 1, name: "газета", icon: "newspaper", AppIcon: "AppIcon 1"),
+            SettingsModel(id: 2, name: "огонь", icon: "fire", AppIcon: "AppIcon 2"),
+            SettingsModel(id: 3, name: "робот", icon: "technology", AppIcon: "AppIcon 3"),
+            SettingsModel(id: 4, name: "мяч", icon: "sport", AppIcon: "AppIcon 4"),
+            SettingsModel(id: 5, name: "монета", icon: "business", AppIcon: "AppIcon 5"),
+            SettingsModel(id: 6, name: "микроскоп", icon: "science", AppIcon: "AppIcon 6"),
+            SettingsModel(id: 7, name: "цирк", icon: "entertainment", AppIcon: "AppIcon 7"),
+            SettingsModel(id: 8, name: "крест", icon: "health", AppIcon: "AppIcon 8")
+        ])
     ]
 }
